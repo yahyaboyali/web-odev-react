fdde217806c7e3970edc76a18ba63a44f37d3818
yahya boyalı <yahyaboyali@gmail.com>
2022-05-15 22:27:17 +0300

    yönetim ekranı düzenleme

--- src/pages/Yonetim.jsx
+++ src/pages/Yonetim.jsx
@@ -1,41 +1,33 @@
 import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row,Card } from 'react-bootstrap'
 import CardLayout from '../layouts/CardLayout'
 export default function Yonetim() {
   return (
-    <div>
-      <Container className='mt-5'>
-        <Row >
-          <label className='bg-light'>
-            <h2>Haberler</h2>
-          </label>
-          <Col>
-            <CardLayout title="Rektör" description="bilgiler " linkHref="#web" linkText="web sayfası" />
-          </Col>
-        </Row>
-        <Row >
-        <Col >
-            <CardLayout title="dekan" description="bilgiler" linkHref="#haber2" linkText="web sayfası" />
-          </Col>
-          <Col >
-            <CardLayout title="rektör yardımcısı" description=" bilgiler" linkHref="#haber3" linkText="web sayfası" />
-          </Col>
-        </Row>
-        <Row className='mt-5' >
-          <label className='bg-light'>
-            <h2>Etkinlikler</h2>
-          </label>
-          <Col >
-            <CardLayout title="danışman" description="bilgiler" linkHref="#duyuru1" linkText="web sayfası" />
-          </Col>
-          <Col >
-            <CardLayout title="sekreter" description="bilgiler" linkHref="#duyuru2" linkText="web sayfası" />
-          </Col>
-          <Col >
-            <CardLayout title="danışman" description="bilgiler" linkHref="#duyuru3" linkText="web sayfası" />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container>
+            <Card body className="m-3">
+                <h1>İdari Birimler</h1>
+            </Card>
+
+            <Card body className="m-3">
+                <Container>
+                    <Row>
+                        <Col>
+                            <CardLayout title="Rektör" description="rektör açıklaması1" />
+                        </Col>
+                    </Row>
+                    <Row className="mt-3">
+                        <Col>
+                            <CardLayout title="Rektör yardımcısı" description="Rektör yardımcısı açıklaması1" />
+                        </Col>
+                        <Col >
+                            <CardLayout title="sekreter" description="sekreter açıklaması2" />
+                        </Col>
+                        <Col >
+                            <CardLayout title="Danışman" description="Danışman açıklaması3" />
+                        </Col>
+                    </Row>
+                </Container>
+            </Card>
+        </Container>
   )
 }
