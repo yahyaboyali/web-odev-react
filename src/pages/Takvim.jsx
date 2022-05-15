f9f1ffaf63c592ad02c496b4e86e9cbaa2ee9e65
ysumut <umut_aslan95@hotmail.com>
2022-05-15 22:17:07 +0300

    Takvim page added

--- src/pages/Takvim.jsx
+++ src/pages/Takvim.jsx
@@ -1,39 +1,21 @@
-import React from 'react'
-import { Card, Container, Col, Row } from 'react-bootstrap'
-import CardLayout from '../layouts/CardLayout'
+import React, { useState } from 'react'
+import { Card, Container, Row } from 'react-bootstrap'
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default function Takvim() {
+    const [value, onChange] = useState(new Date());
+
     return (
         <Container>
             <Card body className="m-3">
                 <h1>Takvim</h1>
             </Card>
 
-            <Card body className="m-3">
-                <Container>
-                    <Row>
-                        <Col>
-                            <CardLayout title="Idari1" description="Idari açıklaması1" />
-                        </Col>
-                        <Col >
-                            <CardLayout title="Idari2" description="Idari açıklaması2" />
-                        </Col>
-                        <Col >
-                            <CardLayout title="Idari3" description="Idari açıklaması3" />
-                        </Col>
-                    </Row>
-                    <Row className="mt-3">
-                        <Col>
-                            <CardLayout title="Idari1" description="Idari açıklaması1" />
-                        </Col>
-                        <Col >
-                            <CardLayout title="Idari2" description="Idari açıklaması2" />
-                        </Col>
-                        <Col >
-                            <CardLayout title="Idari3" description="Idari açıklaması3" />
-                        </Col>
-                    </Row>
-                </Container>
+            <Card body className="m-3" style={{ "margin-left": "auto", "margin-right": "auto" }}>
+                <Row className="justify-content-center">
+                    <Calendar onChange={onChange} value={value} />
+                </Row>
             </Card>
         </Container>
     )
