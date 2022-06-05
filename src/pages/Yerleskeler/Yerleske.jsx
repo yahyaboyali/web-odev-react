a13f5a7bf78a74737d096264c09c3cc3fb8aa800
ysumut <umut_aslan95@hotmail.com>
2022-06-05 19:01:41 +0300

    Yerleskeler fixed

--- src/pages/Yerleskeler/Yerleske.jsx
+++ src/pages/Yerleskeler/Yerleske.jsx
@@ -3,20 +3,28 @@ import { Col, Row, Table, Nav } from 'react-bootstrap'
 import { Link, useParams } from 'react-router-dom';
 import CardLayout from '../../layouts/CardLayout';
 import FacultiesService from '../../services/FacultiesService';
+
 export default function Yerleske() {
     let { id } = useParams();
-    const [Fakulteler, setFakulteler] = useState([])
+    const [Fakulteler, setFakulteler] = useState([]);
+
     useEffect(() => {
         let fakulteService = new FacultiesService();
-        fakulteService.getOneFaculty(id).then(result => setFakulteler(result.data.data))
-    })
+        fakulteService.getOneFaculty(id)
+            .then(result => {
+                console.log('fakulteService', result.data);
+                setFakulteler(result.data.data);
+            })
+            .catch(error => console.log(error));
+    }, []);
 
     return (
         <div>
+            <h1>Yerleşke</h1>
             {
                 Fakulteler.map(Fakulteler => (
                     <Col style={{ marginTop: '0.5rem' }} key={Fakulteler.id}>
-                        <CardLayout title={Fakulteler.name}  linkHref="#haber1" />
+                        <CardLayout title={Fakulteler.name} linkHref="#haber1" />
                     </Col>
                 ))
             }
--- src/services/FacultiesService.js
+++ src/services/FacultiesService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class FacultiesService{
     getOneFaculty(id){
-        return axios.get("https://agalar-university.herokuapp.com/faculty/"+{id})
+        return axios.get("https://agalar-university.herokuapp.com/faculty/" + id)
     }
     
 }
