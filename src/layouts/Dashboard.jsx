ba5343ddf4a685e1af1c55d7a130de95d8726f93
yahya boyalı <yahyaboyali@gmail.com>
2022-06-06 08:53:01 +0300

    gereksiz boş sayfalar silindi

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -3,29 +3,11 @@ import { Routes,Route } from 'react-router-dom'
 
 import MainPage from '../pages/MainPage'
 import Yonetim from '../pages/Yonetim'
-import Bolumler from '../pages/Bolumler'
-import Fakulteler from '../pages/Fakulteler'
 import Misyon from '../pages/Misyon'
 import IdariBirimler from '../pages/IdariBirimler'
 import Kantin from '../pages/Kantin'
 import Yerleskeler from '../pages/Yerleskeler'
 import Takvim from '../pages/Takvim'
-import Muhendislik from '../pages/Fakulteler/Muhendislik'
-import Tip from '../pages/Fakulteler/Tip'
-import Veterinerlik from '../pages/Fakulteler/Veterinerlik'
-import Orman from '../pages/Fakulteler/Orman'
-import SosyalBilimler from '../pages/Fakulteler/SosyalBilimler'
-import BilgisayarMuhendisligi from '../pages/Bolumler/Fakulteler/Muhendislik/BilgisayarMuhendisligi'
-import EndustriMuhendisligi from '../pages/Bolumler/Fakulteler/Muhendislik/EndustriMuhendisligi'
-import InsaatMuhendisligi from '../pages/Bolumler/Fakulteler/Muhendislik/InsaatMuhendisligi'
-import VeterinerHekimligiTemelBilimler from '../pages/Bolumler/Fakulteler/Veteriner/VeterinerHekimligiTemelBilimler'
-import KlinikBilimler from '../pages/Bolumler/Fakulteler/Veteriner/KlinikBilimler'
-import BesinGidaHijyeniTeknolojisi from '../pages/Bolumler/Fakulteler/Veteriner/BesinGidaHijyeniTeknolojisi'
-import OrmanMuhendisligi from '../pages/Bolumler/Orman/OrmanMuhendisligi'
-import OrmanEndustriMuhendisligi from '../pages/Bolumler/Orman/OrmanEndustriMuhendisligi'
-import PeyzajMimarligi from '../pages/Bolumler/Orman/PeyzajMimarligi'
-import Psikoloji from '../pages/Bolumler/Fakulteler/SosyalBilimler/Psikoloji'
-import SporBilimler from '../pages/Fakulteler/SporBilimler'
 import Yerleske from '../pages/Yerleskeler/Yerleske'
 import Department from '../pages/Bolumler/Department'
 export default function Dasboard () {
@@ -34,8 +16,6 @@ export default function Dasboard () {
         <Routes>  
         <Route path="/" element={<MainPage />} />
         <Route path='/Yonetim' element={<Yonetim/>} exact/>
-        <Route path='/Bolumler' element={<Bolumler/>} exact/>
-        <Route path='/Fakulteler' element={<Fakulteler/>} exact/>
         <Route path='/Misyon' element={<Misyon/>} exact/>
         <Route path='/IdariBirimler' element={<IdariBirimler/>} exact/>
         <Route path='/Kantin' element={<Kantin/>} exact/>
@@ -43,22 +23,6 @@ export default function Dasboard () {
         <Route path='/Takvim' element={<Takvim/>} exact/>
         <Route path='/Yerleskeler/:id' element={<Yerleske/>} exact/>
         <Route path='/Department/:id' element={<Department/>} exact/>
-        <Route path='/Fakulteler/Muhendislik' element={<Muhendislik/>} exact/>
-        <Route path='/Fakulteler/Tip' element={<Tip/>} exact/>
-        <Route path='/Fakulteler/SporBilimler' element={<SporBilimler/>} exact/>
-        <Route path='/Fakulteler/Veterinerlik' element={<Veterinerlik/>} exact/>
-        <Route path='/Fakulteler/Orman' element={<Orman/>} exact/>
-        <Route path='/Fakulteler/SosyalBilimler' element={<SosyalBilimler/>} exact/>
-        <Route path='/Bolumler/BilgisayarMuhendisligi' element={<BilgisayarMuhendisligi/>} exact/>
-        <Route path='/Bolumler/EndustriMuhendisligi' element={<EndustriMuhendisligi/>} exact/>
-        <Route path='/Bolumler/InsaatMuhendisligi' element={<InsaatMuhendisligi/>} exact/>
-        <Route path='/Bolumler/KlinikBilimler' element={<KlinikBilimler/>} exact/>
-        <Route path='/Bolumler/VeterinerHekimligiTemelBilimler' element={<VeterinerHekimligiTemelBilimler/>} exact/>
-        <Route path='/Bolumler/BesinGidaHijyeniTeknolojisi' element={<BesinGidaHijyeniTeknolojisi/>} exact/>
-        <Route path='/Bolumler/OrmanMuhendisligi' element={<OrmanMuhendisligi/>} exact/>
-        <Route path='/Bolumler/OrmanEndustriMuhendisligi' element={<OrmanEndustriMuhendisligi/>} exact/>
-        <Route path='/Bolumler/PeyzajMimarligi' element={<PeyzajMimarligi/>} exact/>
-        <Route path='/Bolumler/Psikoloji' element={<Psikoloji/>} exact/>
       </Routes>
     </div>
   )
